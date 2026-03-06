52e338949c12e2dfa069a03b05ba24391ab36c5e
Yohann Bethoule <yohann.bethoule@gmail.com>
2026-03-06 20:39:19 +0100

    Add example and what's next sections to homepage

---
+++ app/components/DevLogList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+export interface DevLogItem {
+  title: string
+  description?: string
+  icon?: string
+}
+
+defineProps<{
+  items: DevLogItem[]
+}>()
+</script>
+
+<template>
+  <UPageGrid>
+    <UPageCard
+      v-for="item in items"
+      :key="item.title"
+      :title="item.title"
+      :description="item.description"
+      :icon="item.icon"
+      variant="soft"
+    />
+  </UPageGrid>
+</template>
