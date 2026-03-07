2be7a62ee13c1ed59467775ddb5c1477f7923a88
Yohann Bethoule <yohann.bethoule@gmail.com>
2026-03-07 13:13:45 +0100

    Update seo image

--- app/app.vue
+++ app/app.vue
@@ -19,8 +19,8 @@ useSeoMeta({
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  ogImage: 'https://deck-passport.vercel.app/homepage_screenshot.png',
+  twitterImage: 'https://deck-passport.vercel.app/homepage_screenshot.png',
   twitterCard: 'summary_large_image'
 })
 </script>
