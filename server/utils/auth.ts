a12521d057d0cfec4c52c057efee3022e3891a55
Yohann Bethoule <yohann.bethoule@gmail.com>
2026-04-16 00:05:44 +0200

    Better auth config for coolify

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -2,22 +2,17 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '../database'
 
-function getBaseURL() {
-  if (process.env.VERCEL_ENV === 'production') {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  }
-  if (process.env.VERCEL_BRANCH_URL) {
-    return `https://${process.env.VERCEL_BRANCH_URL}`
-  }
-  if (process.env.COOLIFY_URL) {
-    console.log('COOLIFY_URL', process.env.COOLIFY_URL)
-    return process.env.COOLIFY_URL
-  }
-  return 'http://localhost:3000'
-}
-
 export const auth = betterAuth({
-  baseURL: getBaseURL(),
+  baseURL: {
+    allowedHosts: [
+      'localhost:3000',
+      '*.vercel.app',
+      '*.deckpassport.com',
+      'deckpassport.com'
+    ],
+    fallback: 'https://deckpassport.com',
+    protocol: import.meta.dev ? 'http' : 'https'
+  },
   database: drizzleAdapter(db, {
     provider: 'pg'
   }),
